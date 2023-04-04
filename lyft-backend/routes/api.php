37cd5d24546802d0896320936b363d8705d67ae1
Lejla Beqiri <lejlabeqiri@hotmail.com>
2023-04-05 01:35:27 +0200

    laravel register and login wip

--- lyft-backend/routes/api.php
+++ lyft-backend/routes/api.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('/register', [RegisterController::class, 'register']);
-Route::group(
-    ['middleware' => ['auth:sanctum']],
-    function () {
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('users', [UserController::class, 'index']);
     }
 );
