1eb4aa504958c417c297844ed2cf252c46d679fb
gresahodolli <gresa.hodolli1@student.uni-pr.edu>
2023-04-11 14:06:33 +0200

    added initial skeleton code for Get a Ride page

--- reapp/reapp/vite-project/src/Pages/Mainride.jsx
+++ reapp/reapp/vite-project/src/Pages/Mainride.jsx
@@ -1,9 +1,72 @@
-export default function Mainride(){
-    return(
+import React, { useState } from 'react';
+import './mainride.css';
+import './styles.css';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+
+function Mainride() {
+    const [isNavbarVisible, setIsNavbarVisible] = useState(true);
+
+    return (
         <div>
-            Mainride
+            <nav className={`${isNavbarVisible ? "visible" : ""}`}>
+                <div className="nav-items">
+                    <button>
+                        Sidebar
+                    </button>
+                    <a>Ulyft</a>
+                </div>
+                <div className="nav-items1">
+                    <button
+                        onClick={() => {
+                            navigate('/mainride');
+                        }}
+                    >
+                        <a>Gresa</a>
+                        <img src="/icons/profile.png" alt="" />
+                    </button>
+                </div>
+            </nav>
+            <div className="container">
+                <div className="input-container">
+                    <h1 className="title">Get a ride in minutes</h1>
+                    <div className="input-group">
+                        <label htmlFor="pickup" className="input-label">
+                            PICKUP LOCATION
+                        </label>
+                        <div className="input-wrapper">
+                            <FaMapMarkerAlt className="input-icon" />
+                            <input
+                                type="text"
+                                id="pickup"
+                                name="pickup"
+                                className="input-field"
+                                placeholder="Enter pickup location"
+                            />
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="destination" className="input-label">
+                            DESTINATION
+                        </label>
+                        <div className="input-wrapper">
+                            <FaMapMarkerAlt className="input-icon" />
+                            <input
+                                type="text"
+                                id="destination"
+                                name="destination"
+                                className="input-field"
+                                placeholder="Enter destination"
+                            />
+                        </div>
+                    </div>
+                    <button className="estimate-button">Get estimate</button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
+export default Mainride;
 
