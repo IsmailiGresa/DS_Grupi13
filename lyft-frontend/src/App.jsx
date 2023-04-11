e7ff3622de3c07307e4864cd51a935d0157f5e48
Lejla Beqiri <lejlabeqiri@hotmail.com>
2023-04-12 00:56:33 +0200

    react app fixed structure

--- lyft-frontend/src/App.jsx
+++ lyft-frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Profile from "./Pages/Profile";
+import Mainride from "./Pages/Mainride";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Router>
+      <nav>
+        <Link to="/mainride"> Mainride </Link>
+        <Link to="/profile"> Profile </Link>
+        </nav>
+      <Routes>
+        <Route path="/mainride" element={<Mainride />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
