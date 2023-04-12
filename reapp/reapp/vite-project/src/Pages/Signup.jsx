937049ba7a1b4f2a2bb9dbe8d82d4c0c9c71223f
altinabytyqi <94831473+altinabytyqi@users.noreply.github.com>
2023-04-12 16:36:31 +0200

    remaining changes

--- reapp/reapp/vite-project/src/Pages/Signup.jsx
+++ reapp/reapp/vite-project/src/Pages/Signup.jsx
@@ -1,9 +1,36 @@
-export default function Signup(){
-    return(
-        <div>
-            Signup
-        </div>
+import React, { useState } from "react";
+import './applog.css';
+
+export const Signup = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+   // const [error,setError]=useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      //  if(name.length==0||pass.length<=8){
+       //     setError(true)
+       // }
+        console.log(email);
+    }
+
+    return (
+        <div> <h2>SignUp</h2>
+        <div className="auth-form-container">
+            
+        <form className="register-form" onSubmit={handleSubmit}>
+           <label htmlFor="name">Full name  </label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="email@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Register</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    </div>
     )
 }
-
-
