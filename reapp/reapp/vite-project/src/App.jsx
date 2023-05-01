7666f7fdecf0cc294fea0f99ea6297e6d609f84d
IsmailiGresa <57864748+IsmailiGresa@users.noreply.github.com>
2023-05-01 14:26:18 +0200

    fix Login/Signup

--- reapp/reapp/vite-project/src/App.jsx
+++ reapp/reapp/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./router.jsx";
--- reapp/reapp/vite-project/src/Pages/Login.jsx
+++ reapp/reapp/vite-project/src/Pages/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import './applog.css';
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
