2f042bd19a4f5ea7ac2a05e774b3171605beb9c1
Lejla Beqiri <lejlabeqiri@hotmail.com>
2023-05-13 17:33:05 +0200

    factory files for user and car

--- lyft-backend/database/factories/UserFactory.php
+++ lyft-backend/database/factories/UserFactory.php
@@ -18,9 +18,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'avatar' => $this->faker->imageUrl(200, 200),
+            'phone_number' => $this->faker->phoneNumber,
+            'birthday' => $this->faker->date,
+            'is_driver' => $this->faker->boolean,
+            'home_address' => $this->faker->address,
+            'work_address' => $this->faker->address,
+            'email' => $this->faker->unique()->safeEmail,
+            'rides' => 0,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
