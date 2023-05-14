cc61ad72d18e43b8f5bcc9e95f831f5cf484a384
IsmailiGresa <57864748+IsmailiGresa@users.noreply.github.com>
2023-05-14 23:00:25 +0200

    Shortcuts table added

---
+++ lyft-backend/database/migrations/2023_05_14_203755_shortcuts.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('shortcuts', function (Blueprint $table) {
+        $table->id();
+        $table->string('home_location');
+        $table->string('work_location');
+        $table->timestamps();
+    });
+}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
