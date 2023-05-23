5e31a6e997cabd17c95c74e89b69a1021b7c52c3
altinabytyqi <94831473+altinabytyqi@users.noreply.github.com>
2023-05-23 17:14:39 +0200

    Update ModalPay.jsx

--- reapp/reapp/vite-project/src/Pages/ModalPay.jsx
+++ reapp/reapp/vite-project/src/Pages/ModalPay.jsx
@@ -1,48 +1,48 @@
-import React from "react";
-import "./ModalPay.css";
+// import React from "react";
+// import "./ModalPay.css";
 
-function ModalPay({ setOpenModal4 }) {
-    return (
-        <div className="modalBackground">
-            <div className="modalContainer">
-                <div className="titleCloseBtn">
-                    <button className="close"
-                            onClick={() => {
-                                setOpenModal4(false);
-                            }}
-                    >
-                        x
-                    </button>
-                </div>
-                <div className="title">
-                    Edit account information
-                </div>
-                <div className="upload">
-                    <form>
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" placeholder="Enter your name..."></input>
+// function ModalPay({ setOpenModal4 }) {
+//     return (
+//         <div className="modalBackground">
+//             <div className="modalContainer">
+//                 <div className="titleCloseBtn">
+//                     <button className="close"
+//                             onClick={() => {
+//                                 setOpenModal4(false);
+//                             }}
+//                     >
+//                         x
+//                     </button>
+//                 </div>
+//                 <div className="title">
+//                     Edit account information
+//                 </div>
+//                 <div className="upload">
+//                     <form>
+//                         <label for="name">Name:</label>
+//                         <input type="text" id="name" placeholder="Enter your name..."></input>
 
-                        <label for="surname">Last Name:</label>
-                        <input type="text" id="surname" placeholder="Enter your lastname..."></input>
+//                         <label for="surname">Last Name:</label>
+//                         <input type="text" id="surname" placeholder="Enter your lastname..."></input>
 
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" placeholder="Enter your email address..."></input>
-                    </form>
-                </div>
-                <div className="footer">
-                    <button
-                        onClick={() => {
-                            setOpenModal4(false);
-                        }}
-                        id="cancelBtn"
-                    >
-                        Cancel
-                    </button>
-                    <button>Update</button>
-                </div>
-            </div>
-        </div>
-    );
-}
+//                         <label for="email">Email:</label>
+//                         <input type="email" id="email" placeholder="Enter your email address..."></input>
+//                     </form>
+//                 </div>
+//                 <div className="footer">
+//                     <button
+//                         onClick={() => {
+//                             setOpenModal4(false);
+//                         }}
+//                         id="cancelBtn"
+//                     >
+//                         Cancel
+//                     </button>
+//                     <button>Update</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default ModalPay;
+// export default ModalPay;
