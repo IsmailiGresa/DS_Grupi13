e646b8707b7749b8a243825a01e12b440d6deb8a
IsmailiGresa <57864748+IsmailiGresa@users.noreply.github.com>
2023-05-23 12:38:26 +0200

    Adding proper image upload

--- lyft-frontend/src/Pages/Modal.jsx
+++ lyft-frontend/src/Pages/Modal.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css"
+import Axios from "axios";
 
 function Modal({ setOpenModal }) {
+
+  const[imagedata, setImagedata] = useState('');
+  const handleChange = file => {
+    setImagedata(file[0]);
+  }
+  const sumbitData = e => {
+    e.preventDefault();
+    const fData = new FormData();
+    fData.append('image', imagedata);
+
+    Axios.post('http://127.0.0.1:8000/api/upload-image', fData)
+    .then(res => {
+      console.log('response', res);
+    }).catch(e => {
+      console.error('Fail', e);
+    });
+  }
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -17,11 +36,11 @@ function Modal({ setOpenModal }) {
         <div className="title">
           Upload a photo
         </div>
-        <div className="upload">
-            <button>
-                <img src="/icons/image-.png"></img>
-                <div>Click here or drag and drop to choose a photo.</div>
-            </button>
+        <div className="upl" onSubmit={sumbitData}>
+              <img src="/icons/image-.png"></img>
+              <input name="image" id="image" type="file" onChange={e => handleChange(e.target.files)}></input>
+                
+          
         </div>
         <div className="footer">
           <button
@@ -32,7 +51,7 @@ function Modal({ setOpenModal }) {
           >
             Cancel
           </button>
-          <button>Confirm</button>
+          <button type="submit" onClick={sumbitData}>Confirm</button>
         </div>
       </div>
     </div>
