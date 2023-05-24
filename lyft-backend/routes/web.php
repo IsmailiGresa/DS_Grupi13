7bb0bb3301313d905340fa6b3e448805e0669930
ArbreshaHodolli <arberesha.hodolli@student.uni-pr.edu>
2023-05-24 14:09:32 +0200

    update router folders

--- lyft-backend/routes/web.php
+++ lyft-backend/routes/web.php
@@ -15,4 +15,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\GiftCardHistoryController;
+use App\Http\Controllers\InviteHistoryController;
 
+
+
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/gift-card-history', [GiftCardHistoryController::class, 'index']);
+    Route::post('/gift-card-history', [GiftCardHistoryController::class, 'store']);
+    Route::get('/invite-history', [InviteHistoryController::class, 'index']);
+    Route::post('/invite-history', [InviteHistoryController::class, 'store']);
+    Route::get('/invite-history/{id}', [InviteHistoryController::class, 'show']);
+    Route::put('/invite-history/{id}', [InviteHistoryController::class, 'update']);
+    Route::delete('/invite-history/{id}', [InviteHistoryController::class, 'destroy']);
+});
