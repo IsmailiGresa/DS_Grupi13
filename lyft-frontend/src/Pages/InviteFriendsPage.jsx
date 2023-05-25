3f074c27c7be9a3af2b45b43fc10d25e9595e875
ArbreshaHodolli <arberesha.hodolli@student.uni-pr.edu>
2023-05-25 12:22:37 +0200

    fixing errors for Invitefriends

--- lyft-frontend/src/Pages/InviteFriendsPage.jsx
+++ lyft-frontend/src/Pages/InviteFriendsPage.jsx
@@ -1,38 +1,48 @@
 import React, { useState } from 'react';
-import './inviteFriends.css';
-import React, { useState } from 'react';
 import axios from 'axios';
 import './inviteFriends.css';
+
 function InviteFriendsPage() {
-    const [selectedTimeRange, setSelectedTimeRange] = useState("lastWeek1");
+    const [selectedTimeRange, setSelectedTimeRange] = useState('lastWeek1');
     const [showLastWeekContent, setShowLastWeekContent] = useState(false);
     const [showLast30DaysContent, setShowLast30DaysContent] = useState(false);
     const [inviteCodeValue, setInviteCodeValue] = useState(generateInviteCode()); // Generate random invite code
 
     const handleCopyInviteCode = () => {
-        const copyText = document.getElementById("invite-code1");
+        const copyText = document.getElementById('invite-code1');
         copyText.select();
         copyText.setSelectionRange(0, 99999);
-        document.execCommand("copy");
+        document.execCommand('copy');
 
-        // Dërgoni të dhënat në backend
-        axios.post('/api/invite-history', {
-            code: copyText.value,
-            date: new Date(),
-            applications: 0,
-            activations: 0,
-            earnings: 0,
-        })
-            .then(response => {
-                // Sukses
+        // Send data to the backend
+        axios
+            .post('/api/invite-history', {
+                code: copyText.value,
+                date: new Date(),
+                applications: 0,
+                activations: 0,
+                earnings: 0,
+            })
+            .then((response) => {
+                // Success
                 console.log(response.data);
             })
-            .catch(error => {
-                // Gabim gjatë dërgimit të të dhënave
+            .catch((error) => {
+                // Error while sending data
                 console.error(error);
             });
 
-        alert("Copied the text: " + copyText.value);
+        alert('Copied the text: ' + copyText.value);
+    };
+
+    const generateInviteCode = () => {
+        // Generate and return the invite code
+        // Replace this with your own implementation
+        return 'ABC123';
+    };
+
+    const handleTimeRangeChange = (timeRange) => {
+        setSelectedTimeRange(timeRange);
     };
 
     return (
@@ -43,34 +53,31 @@ function InviteFriendsPage() {
                 <label htmlFor="invite-code1">Your invite code</label>
                 <input type="text" id="invite-code1" value={inviteCodeValue} readOnly />
 
-                <form method="POST" action="/your-endpoint">
-                    @csrf
-                </form><button className="button1" onClick={handleCopyInviteCode}>
+                <button className="button1" onClick={handleCopyInviteCode}>
                     Copy invite
                 </button>
-                </form>
-            </div>
+            </form>
 
             <h1 className="invite-historyy">Your invite history</h1>
 
             <div className="time-range-switch1">
                 <button
                     onClick={() => {
-                        handleTimeRangeChange("lastWeek1");
+                        handleTimeRangeChange('lastWeek1');
                         setShowLastWeekContent(true);
                         setShowLast30DaysContent(false);
                     }}
-                    className={selectedTimeRange === "lastWeek1" ? "active" : ""}
+                    className={selectedTimeRange === 'lastWeek1' ? 'active' : ''}
                 >
                     Last week
                 </button>
                 <button
                     onClick={() => {
-                        handleTimeRangeChange("last30Days1");
+                        handleTimeRangeChange('last30Days1');
                         setShowLastWeekContent(false);
                         setShowLast30DaysContent(true);
                     }}
-                    className={selectedTimeRange === "last30Days1" ? "active" : ""}
+                    className={selectedTimeRange === 'last30Days1' ? 'active' : ''}
                 >
                     Last 30 days
                 </button>
@@ -84,8 +91,12 @@ function InviteFriendsPage() {
                     <div className="data-row1">
                         <div className="data-cell1">Date</div>
                         <div className="data-cell1">Code</div>
-                        <div className="data-cell1">Applications<span className="registered-symbol1">&reg;</span></div>
-                        <div className="data-cell1">Activations<span className="registered-symbol11">&reg;</span></div>
+                        <div className="data-cell1">
+                            Applications<span className="registered-symbol1">&reg;</span>
+                        </div>
+                        <div className="data-cell1">
+                            Activations<span className="registered-symbol11">&reg;</span>
+                        </div>
                         <div className="data-cell1">Earnings</div>
                     </div>
                 </div>
@@ -99,8 +110,12 @@ function InviteFriendsPage() {
                     <div className="data-row1">
                         <div className="data-cell1">Date</div>
                         <div className="data-cell1">Code</div>
-                        <div className="data-cell1">Applications<span className="registered-symbol1">&reg;</span></div>
-                        <div className="data-cell1">Activations<span className="registered-symbol11">&reg;</span></div>
+                        <div className="data-cell1">
+                            Applications<span className="registered-symbol1">&reg;</span>
+                        </div>
+                        <div className="data-cell1">
+                            Activations<span className="registered-symbol11">&reg;</span>
+                        </div>
                         <div className="data-cell1">Earnings</div>
                     </div>
                 </div>
