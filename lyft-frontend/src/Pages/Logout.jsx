6a149cb3b983c9a4a9063ec710c45527d470f658
altinabytyqi <94831473+altinabytyqi@users.noreply.github.com>
2023-05-26 21:18:20 +0200

    updates

--- lyft-frontend/src/Pages/Logout.jsx
+++ lyft-frontend/src/Pages/Logout.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import axios from 'axios';
 
 const Logout = () => {
@@ -49,5 +50,29 @@ export default Logout;
 //         </div>
 //     )
 // }
+=======
+import React from 'react';
+import axios from 'axios';
+>>>>>>> Stashed changes
 
+const Logout = () => {
+    const handleLogout = async () => {
+        try {
+            // Send a logout request to the backend
+            await axios.post('/api/logout');
+            // Clear any local storage 
+            localStorage.removeItem('token');
+            // Redirect the user to the login page
+            window.location.href = '/login';
+        } catch (error) {
+            // Handle error if the logout request fails
+            console.error('Logout failed:', error);
+        }
+    };
 
+    return (
+        <button onClick={handleLogout}>Logout</button>
+    );
+};
+
+export default Logout;
